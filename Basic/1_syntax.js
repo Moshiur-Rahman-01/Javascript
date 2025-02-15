9f3fb72dd3aecd2873072765931e9117395741c1
Moshiur-Rahman-01 <moshiur.ict24@gmail.com>
2025-02-15 23:03:05 +0600

    added

---
+++ Basic/1_syntax.js
@@ -0,0 +1,32 @@
+// test-1
+let x;
+x = 6;
+console.log(x);
+
+// test-2
+console.log((5+6)*10);
+console.log(5*10);
+console.log(x*10);
+console.log("John" + " " + "Doe");
+
+// test-3
+let a,b;
+a = 5;
+b = 6;
+console.log(a,b);
+
+// test-4
+let c, d;
+c = 5 + 6;
+d = c * 10;
+console.log(d);
+
+// test-5
+let person = "John Doe",
+carName = "Volvo",
+price = 200;
+console.log(price);
+
+// test-6
+let carName2;
+console.log(carName2);
---
+++ Basic/5_Arithmetic.js
@@ -0,0 +1,15 @@
+// test-1
+let x = 5 + 2 + 3;
+console.log(x);
+
+// test-2
+let str = "John" + " " + "Doe";
+console.log(str);
+
+// test-3
+let a = "5" + 2 + 3;
+console.log(a); // 1st element onujayee datatype hobe...
+
+// test-4
+let b = 2 + 3 + "5";
+console.log(b); // 55
